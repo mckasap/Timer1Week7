672d1b67a954a36161774574cd8fe0496ec3cb7a
mckasap <mckasap@gmail.com>
2020-04-02 01:14:56 +0300

    CountDownTimer ile yapıldı işler

--- app/src/main/java/org/kasapbasi/timer1week7/MainActivity.java
+++ app/src/main/java/org/kasapbasi/timer1week7/MainActivity.java
@@ -3,6 +3,7 @@ package org.kasapbasi.timer1week7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.util.Log;
 
@@ -12,16 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Handler myHandler= new Handler();
+        final CountDownTimer countDownTimer = new CountDownTimer(10000, 1000) {
+            /**
+             * Callback fired on regular interval.
+             *
+             * @param millisUntilFinished The amount of time until finished.
+             */
+            @Override
+            public void onTick(long millisUntilFinished) {
+                Log.d("Tick","Countdown in progres "+(millisUntilFinished)/1000+"left");
+            }
+
+            /**
+             * Callback fired when the time is up.
+             */
+            @Override
+            public void onFinish() { Log.d("Fin","Countdown finished");}
+        };
+        countDownTimer.start();
+        /*final Handler myHandler= new Handler();
         Runnable myrun = new Runnable() {
             @Override
             public void run() {
-
                 Log.d("sonuc","Bir saniye geçti");
                 myHandler.postDelayed(this,1000); //belirtilen ms bekler çalıştırır.
                     //Burada kendi kendini çalıştırıyor.
             }
         };
-        myHandler.post(myrun); // anında çalıştırır
+        myHandler.post(myrun); // anında çalıştırır*/
+
     }
 }
