f0403ca867f54fb64e9888514447f4ba36bb8a2e
mckasap <mckasap@gmail.com>
2020-04-02 00:00:10 +0300

    Handler ekledi log Ekranında timer çalışması sağlandı.

--- app/src/main/java/org/kasapbasi/timer1week7/MainActivity.java
+++ app/src/main/java/org/kasapbasi/timer1week7/MainActivity.java
@@ -3,6 +3,8 @@ package org.kasapbasi.timer1week7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Handler myHandler= new Handler();
+        Runnable myrun = new Runnable() {
+            @Override
+            public void run() {
+
+                Log.d("sonuc","Bir saniye geçti");
+                myHandler.postDelayed(this,1000); //belirtilen ms bekler çalıştırır.
+                    //Burada kendi kendini çalıştırıyor.
+            }
+        };
+        myHandler.post(myrun); // anında çalıştırır
     }
 }
